148668015571ecb30286b376d5cc634f09654e32
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-11 21:31:09 +0300

    compleat Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Program shows all even nubers from 1 till N");
+
+System.Console.Write("Enter your N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i < n +1)
+{
+    if (i%2 == 0)
+    {
+        System.Console.Write($" {i} ");        
+    }
+    i++;    
+}
+
