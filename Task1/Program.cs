cbbe89c72f37f8fc27b6a58fdd0993432f73819b
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-11 16:08:30 +0300

    Compleated Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("This program gives maximum and minumum value out of two values");
+System.Console.WriteLine("Enter your 1st value");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter your 2nd value");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+if (value1 != value2)
+{
+    if (value1 > value2)
+    {
+        System.Console.WriteLine($" MAX = {value1}, MIN = {value2}");
+    }
+    else
+    {
+        System.Console.WriteLine($"MAX = {value2} , MIN = {value1}");
+    }
+}
+else
+{
+    System.Console.WriteLine($"{value2} = {value1} => there is no MIN/MAX");
+}
