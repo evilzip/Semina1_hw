b7aa27cd4b29b7f9ae6ca5e1398ef9a377da6ca0
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-11 20:42:43 +0300

    compleat Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("This program checks if your value is EVEN");
+
+System.Console.Write("Enter your value ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value%2 == 0)
+{
+    System.Console.WriteLine($"Yes,{value} is EVEN");
+
+}
+else
+{
+    System.Console.WriteLine($"No, {value} is not EVEN");
+
+}
